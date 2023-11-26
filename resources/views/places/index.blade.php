45e22e32ab27f284facbb005f9c312ef51c4831f
Audy Cipta <audycipta02@gmail.com>
2023-11-26 22:29:08 +0800

    refactor place page

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -19,11 +19,27 @@
   <section id="places">
     <div class="container">
 
-      <div class="section-header row justify-content-center">
-        <div class="col-7">
-          <h2 class="text-center">Popular Places</h2>
-          <p class="text-center mb-0">Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum
-            tempus hac tellus libero accumsan.</p>
+      <div class="row section-header justify-content-center">
+        <div class="col-9">
+          <h2 class="text-center">Your Best Place for Unforgettable Experiences!</h2>
+          <p class="text-center mb-0">Explore perfection in Badung, epic bucket list to <br> add your to next itinerary
+          </p>
+        </div>
+      </div>
+
+      <div class="row mt-5 mb-5 justify-content-center">
+        <div class="col-md-4">
+          <div class="input-group">
+            <input type="text" class="form-control rounded-pill" placeholder="Recipient's username"
+              aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <span class="input-group-text" id="basic-addon2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-search" viewBox="0 0 16 16">
+                <path
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
+            </span>
+          </div>
         </div>
       </div>
 
