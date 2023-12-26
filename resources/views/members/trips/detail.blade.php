00f856878e8514ffcb8f568108e1597b85d20f43
Abel Yoshuara <abelyoshuara@gmail.com>
2023-12-26 16:09:03 +0800

    update my-trip detail page

--- resources/views/members/trips/detail.blade.php
+++ resources/views/members/trips/detail.blade.php
@@ -140,14 +140,14 @@
       <p class="text-white lead">{{ date('m d, Y', strtotime($itineraryBook->start_day)) }} -
         {{ date('m d, Y', strtotime($itineraryBook->start_day . ' + ' . $itineraryBook->total_day - 1 . ' days')) }}
       </p>
-      <button class="btn btn-primary py-2 px-3 mt-2 rounded-pill d-flex align-items-center mx-auto" data-bs-toggle="modal"
+      <button class="btn btn-danger py-2 px-3 mt-2 rounded-pill d-flex align-items-center mx-auto" data-bs-toggle="modal"
         data-bs-target="#deleteItineraryModal">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
           class="bi bi-trash-fill" viewBox="0 0 16 16">
           <path
             d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
         </svg>
-        <span class="ms-2">Delete</span>
+        <span class="ms-2">Delete This Trip</span>
       </button>
     </div>
   </section>
