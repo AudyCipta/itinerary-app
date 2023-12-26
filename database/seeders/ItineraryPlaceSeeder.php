3abf1b6ea3454bee6fdabe9ec4f20c77ff05ffe5
Abel Yoshuara <abelyoshuara@gmail.com>
2023-12-26 16:58:52 +0800

    update seeder

--- database/seeders/ItineraryPlaceSeeder.php
+++ database/seeders/ItineraryPlaceSeeder.php
@@ -14,6 +14,7 @@ class ItineraryPlaceSeeder extends Seeder
     public function run(): void
     {
         DB::table('itinerary_places')->insert([
+            // Itinerary 2
             [
                 'itinerary_id' => 1,
                 'place_id' => 34,
@@ -24,22 +25,54 @@ class ItineraryPlaceSeeder extends Seeder
             ],
             [
                 'itinerary_id' => 1,
-                'place_id' => 2,
-                'day_to' => 2,
-                'time' => '20:30',
+                'place_id' => 35,
+                'day_to' => 1,
+                'time' => '16:00',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'itinerary_id' => 1,
                 'place_id' => 3,
-                'day_to' => 2,
+                'day_to' => 1,
                 'time' => '14:30',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 'itinerary_id' => 1,
+                'place_id' => 3,
+                'day_to' => 1,
+                'time' => '14:30',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 3,
+                'day_to' => 1,
+                'time' => '14:30',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 3,
+                'day_to' => 1,
+                'time' => '14:30',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 3,
+                'day_to' => 1,
+                'time' => '14:30',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 2,
                 'place_id' => 4,
                 'day_to' => 3,
                 'time' => '16:30',
@@ -47,10 +80,361 @@ class ItineraryPlaceSeeder extends Seeder
                 'updated_at' => now()
             ],
             [
-                'itinerary_id' => 1,
+                'itinerary_id' => 2,
                 'place_id' => 5,
                 'day_to' => 3,
                 'time' => '09:30',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 4,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 4,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 4,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 4,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+
+            // Itinerary 2
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+
+            // Itinerary 3
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+
+            // Itinerary 4
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+
+            // Itinerary 5
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+
+
+            // Itinerary 6
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'itinerary_id' => 1,
+                'place_id' => 6,
+                'day_to' => 3,
+                'time' => '09:31',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
